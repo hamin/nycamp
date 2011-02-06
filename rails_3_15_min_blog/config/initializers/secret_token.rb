352b8eae1d3f0854cf0ef2f415a5b309df1204c4
Haris Amin <aminharis7@gmail.com>
2011-02-05 22:09:48 -0500

    Rails 3 15 min Blog tutuorial for NYCamp

---
+++ rails_3_15_min_blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rails315MinBlog::Application.config.secret_token = 'ab6cbeb6814958cbe2b056dae82d12c41119133544559d5f2c78a57defcf04b1c14acd5d2b8e89d019534a83846d39bc2f4abfc44c4856be294cf31c3b5326ac'
